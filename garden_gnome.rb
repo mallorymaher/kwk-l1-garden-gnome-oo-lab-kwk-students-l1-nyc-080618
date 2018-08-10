fae87796152b05a4839034968ff923aa8f02cd92
mallorymaher <mallory.maher@comcast.net>
2018-08-10 14:41:04 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,6 +1,7 @@
 class GardenGnome
-  def initialize ()
-    @personality = evil
+  def initialize (personality= "evil", hat_color= "red")
+    @personality = personality
+    @hat_color = hat_color
   end 
   
   def name
@@ -26,6 +27,21 @@ class GardenGnome
   def gluten_allergy=(new_gluten)
     @gluten_allergy=new_gluten
   end
+  
+  def personality
+    @personality
+  end
+  
+  def hat_color
+    @hat_color
+  end
+  
+  def hat_color=(new_hat)
+    @hat_color= new_hat
+  end
+  
+  def gnaw
+    return 
 
 end
 
